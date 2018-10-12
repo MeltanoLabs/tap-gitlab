94f42bcf9c87f14d7605c7e489eb5a72defafd45
Jacob Schatz <jschatz@gitlab.com>
2018-10-12 19:47:56 -0400

    Add error when not authorized

--- first_tap.py
+++ first_tap.py
@@ -41,8 +41,11 @@ REQUIRED_CONFIG_KEYS = [
 args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 api_key = args.config.get('api_key')
 gitlab_runners_url = f'https://gitlab.com/api/v4/runners/?private_token={api_key}'
-
 resp = requests.get(gitlab_runners_url)
+status_code = resp.status_code
+resp = resp.json()
+if status_code != 200:
+  raise requests.HTTPError(resp)
 singer.write_schema('runners', schema, 'id')
-singer.write_records('runners', resp.json())
+singer.write_records('runners', resp)
 
