db77b934e9d051fa1064eec6300e96f8fa256c6b
mbergeron <mbergeron@gitlab.com>
2018-10-25 13:31:49 -0400

    fixing the setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name='tap-first',
   version='1.0',
-  py_modules=['tap-first'],
+  py_modules=['tap_first'],
   install_requires=[
     'requests',
     'singer-python',
   ],
   entry_points='''
     [console_scripts]
-    tap-first=first_tap
+    tap-first=tap_first:main
   '''
 )
--- tap_first.py
+++ tap_first.py
@@ -38,14 +38,17 @@ REQUIRED_CONFIG_KEYS = [
   'api_key',
 ]
 
-args = utils.parse_args(REQUIRED_CONFIG_KEYS)
-api_key = args.config.get('api_key')
-gitlab_runners_url = f'https://gitlab.com/api/v4/runners/?private_token={api_key}'
-resp = requests.get(gitlab_runners_url)
-status_code = resp.status_code
-resp = resp.json()
-if status_code != 200:
-  raise requests.HTTPError(resp)
-singer.write_schema('runners', schema, 'id')
-singer.write_records('runners', resp)
+
+def main():
+    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+    api_key = args.config.get('api_key')
+    gitlab_runners_url = f'https://gitlab.com/api/v4/runners/?private_token={api_key}'
+    resp = requests.get(gitlab_runners_url)
+    status_code = resp.status_code
+    resp = resp.json()
+    if status_code != 200:
+        raise requests.HTTPError(resp)
+
+    singer.write_schema('runners', schema, 'id')
+    singer.write_records('runners', resp)
 
