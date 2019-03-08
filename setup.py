04150f4cda8be0db6b9d27c11814b3acb7bbfcdc
Yannis Roussos <iroussos@gitlab.com>
2019-03-08 21:29:43 +0200

    Use Singer.io's tap-gitlab implementation as the starting point for this tap

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
-setup(
-  name='tap-gitlab',
-  version='1.0',
-  py_modules=['tap_gitlab'],
-  install_requires=[
-    'requests',
-    'singer-python',
-  ],
-  entry_points='''
-    [console_scripts]
-    tap-gitlab=tap_gitlab:main
-  '''
+setup(name='tap-gitlab',
+      version='0.5.1',
+      description='Singer.io tap for extracting data from the GitLab API',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_gitlab'],
+      install_requires=[
+          'singer-python==5.0.4',
+          'requests==2.20.0',
+          'strict-rfc3339==0.7',
+          'backoff==1.3.2'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-gitlab=tap_gitlab:main
+      ''',
+      packages=['tap_gitlab'],
+      package_data = {
+          'tap_gitlab/schemas': [
+            "branches.json",
+            "commits.json",
+            "issues.json",
+            "milestones.json",
+            "projects.json",
+            "users.json",
+          ],
+      },
+      include_package_data=True,
 )
