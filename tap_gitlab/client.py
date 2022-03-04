5ccb5f9e861b266af4bfd1606f08602458fa26e2
Aaron ("AJ") Steers <aj@meltano.com>
2022-03-04 10:30:35 -0800

    records streaming!

--- tap_gitlab/client.py
+++ tap_gitlab/client.py
@@ -17,7 +17,7 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 class GitLabStream(RESTStream):
     """GitLab stream class."""
 
-    url_base = "https://api.mysample.com"
+    url_base = "https://gitlab.com/api/v4"
 
     # OR use a dynamic url_base:
     # @property
