a751843823a67598f776d5bc27facd124b26e55d
Laurent Savaete <laurent@where.tf>
2022-03-16 10:15:55 +0000

    Fix linter errors

--- tap_gitlab/tests/test_core.py
+++ tap_gitlab/tests/test_core.py
@@ -15,10 +15,7 @@ SAMPLE_CONFIG = {
 # Run standard built-in tap tests from the SDK:
 def test_standard_tap_tests():
     """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(
-        TapGitLab,
-        config=SAMPLE_CONFIG
-    )
+    tests = get_standard_tap_tests(TapGitLab, config=SAMPLE_CONFIG)
     for test in tests:
         test()
 
--- tap_gitlab/transforms.py
+++ tap_gitlab/transforms.py
@@ -1,9 +1,12 @@
+"""Utility functions."""
 from typing import Any, Dict, List
 
 
 def object_array_to_id_array(items: List[dict]) -> List[int]:
+    """Extract id from nested array of objects."""
     return [item["id"] for item in items]
 
 
 def pop_nested_id(record: Dict[str, Any], key: str) -> List[int]:
+    """Extract id from nested owner object and removes the object."""
     return (record.pop("owner", None) or {}).pop("id", None)
