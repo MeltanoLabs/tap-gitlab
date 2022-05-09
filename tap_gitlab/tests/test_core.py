fe976068015895e6e92872a4a496633af28f35ca
Aaron ("AJ") Steers <aj@meltano.com>
2022-05-09 00:19:13 -0700

    Fix CI (#76)
    * Ignore sonarcloud code duplication errors
    * update env var maps, fix defaults
    * bump sdk versin
    * bump black version
    
    Co-authored-by: Laurent Savaete <laurent@where.tf>

--- tap_gitlab/tests/test_core.py
+++ tap_gitlab/tests/test_core.py
@@ -2,16 +2,30 @@
 
 import datetime
 import os
+from typing import Any, Dict
 
+from dotenv import load_dotenv
 from singer_sdk.testing import get_standard_tap_tests
 
-from tap_gitlab.tap import TapGitLab
+from tap_gitlab.tap import OPTIN_STREAM_NAMES, TapGitLab
 
-SAMPLE_CONFIG = {
+load_dotenv()  # Import any environment variables from local `.env` file.
+
+PREFIX = "TAP_GITLAB_"
+SAMPLE_CONFIG: Dict[str, Any] = {
     "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    "private_token": os.getenv("GITLAB_PRIVATE_TOKEN"),
-    "projects": os.getenv("GITLAB_PROJECTS_TO_FETCH", ""),
+    "private_token": os.getenv("TAP_GITLAB_PRIVATE_TOKEN"),
+    "projects": os.getenv("TAP_GITLAB_PROJECTS", "meltano/demo-project"),
+    "groups": os.getenv("TAP_GITLAB_GROUPS", "meltano/infra"),
 }
+for k, v in os.environ.items():
+    if k.startswith(PREFIX):
+        if v.lower() == "false":
+            os.environ.pop(k)
+            SAMPLE_CONFIG[k.lstrip(PREFIX).lower()] = False
+        elif v.lower() == "true":
+            os.environ.pop(k)
+            SAMPLE_CONFIG[k.lstrip(PREFIX).lower()] = True
 
 
 # Run standard built-in tap tests from the SDK:
@@ -22,4 +36,9 @@ def test_standard_tap_tests():
         test()
 
 
-# TODO: Create additional tests as appropriate for your tap.
+#       https://gitlab.com/meltano/sdk/-/merge_requests/265
+def test_tap_config_defaults():
+    """Run standard tap tests from the SDK."""
+    tap = TapGitLab(config=SAMPLE_CONFIG, parse_env_config=True)
+    for optin_stream in OPTIN_STREAM_NAMES:
+        assert f"fetch_{optin_stream}" in tap.config
